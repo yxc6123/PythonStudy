412b4f1feba516d4b947bf7be2a8678e0d217600
yangso <yxc6123@qq.com>
2018-09-22 21:15:32 +0800

    添加浏览器其传入参数使用

--- hello.py
+++ hello.py
@@ -3,9 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+#测试根目录
+#localhost:5000
 @app.route('/')
 def index():
 	return 'Hello World!</h1>'
+
+#测试添加参数
+#<name>为浏览器传入的参数
+@app.route('/user/<name>')
+def user(name):
+	return 'Hello, %s' % name
 
 
 if __name__ == '__main__':
