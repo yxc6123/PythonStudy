b6a9129b30dd951939291fb20f52016f9d9166f0
yangso <yxc6123@qq.com>
2018-09-24 21:28:39 +0800

    Form使用
    Signed-off-by: yangso <yxc6123@qq.com>

--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret_key for yangcc'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/web'
 manager = Manager(app)
 
 #创建bootstrap对象
@@ -13,11 +19,47 @@ bootstrap = Bootstrap(app)
 #创建moment
 moment = Moment(app)
 
+#创建数据库
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+	__tablename = 'roles'
+	id = db.Column(db.Integer,primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+
+class User(db.Model):
+	__tablename = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+
+class NameForm(FlaskForm):
+	name = StringField('What is your name?', validators=[DataRequired()])
+	submit = SubmitField('Submit')
+
+
 #测试根目录
 #localhost:5000
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', current_time=datetime.utcnow())
+	form = NameForm()
+	if form.validate_on_submit():
+		old_name = session.get('name')
+		if old_name is not None and old_name != form.name.data:
+			flash('Looks like you have changed your name!')
+		session['name'] = form.name.data
+		return redirect(url_for('index'))
+	return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 #测试添加参数
 #<name>为浏览器传入的参数
@@ -35,6 +77,6 @@ def internal_server_error(e):
 	return render_template('500.html'), 500
 
 if __name__ == '__main__':
-	manager.run()
+	app.run()
 
 
