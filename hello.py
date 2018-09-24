082fe3c548e235caafc11a2f344fcbb3704ad534
yangso <yxc6123@qq.com>
2018-09-24 22:44:34 +0800

    数据库迁移

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template,session, redirect, url_for, flash
-from flask_script import Manager
+from flask_script import Manager,Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -7,6 +7,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate,MigrateCommand
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key for yangcc'
@@ -22,6 +23,8 @@ moment = Moment(app)
 #创建数据库
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 class Role(db.Model):
 	__tablename__ = 'roles'
@@ -47,6 +50,10 @@ class NameForm(FlaskForm):
 	name = StringField('What is your name?', validators=[DataRequired()])
 	submit = SubmitField('Submit')
 
+@app.shell_context_processor
+def make_shell_context():
+	return dict(db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 #测试根目录
 #localhost:5000
@@ -82,6 +89,6 @@ def internal_server_error(e):
 	return render_template('500.html'), 500
 
 if __name__ == '__main__':
-	app.run()
+	manager.run()
 
 
